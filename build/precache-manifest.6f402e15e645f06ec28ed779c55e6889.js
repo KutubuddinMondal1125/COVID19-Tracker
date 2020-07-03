62ef8d002efe70ef9db328e6737ba21e3074e3da
Kutub <kutubuddinmondal512@gmail.com>
2020-07-03 20:16:51 +0530

    coronavirustracker App is deployed successfully on Netlify

---
+++ build/precache-manifest.6f402e15e645f06ec28ed779c55e6889.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d739cb4073e07310853abbf21ee28f8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8f1f68c9db9dcc1bd6c",
+    "url": "/static/css/main.0f5bed99.chunk.css"
+  },
+  {
+    "revision": "cdaa4a06ec072095d613",
+    "url": "/static/js/2.438f6f63.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.438f6f63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8f1f68c9db9dcc1bd6c",
+    "url": "/static/js/main.ed878464.chunk.js"
+  },
+  {
+    "revision": "a5ddf7359d780a23adaa",
+    "url": "/static/js/runtime-main.9866c732.js"
+  }
+]);
